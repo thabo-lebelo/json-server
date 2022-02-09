a5f4a1b6b1d3a3117c4c9b0518382b30b33b416e
Thabo Lebelo <thabolebelo@icloud.com>
2022-02-09 22:43:26 +0200

    Fetch single post

---
+++ js/details.js
@@ -0,0 +1,19 @@
+const id = new URLSearchParams(window.location.search).get('id');
+const container = document.querySelector('.details');
+
+const renderDetails = async () => {
+    let uri = `http://localhost:3000/posts/${id}`;
+
+    const res = await fetch(uri);
+    const post = await res.json();
+    console.log(post)
+
+    const template = `
+        <h1>${post.title}</h1>
+        <p>${post.body}</p>
+    `;
+
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', () => renderDetails());
